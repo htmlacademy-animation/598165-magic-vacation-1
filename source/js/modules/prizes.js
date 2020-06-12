b2e13f58c3fcebdd0cd77eafa3703809de2c2170
Konstantin Surikov <surikov@gmail.com>
2020-06-12 10:22:05 +0300

    Fixed travis test

--- source/js/modules/prizes.js
+++ source/js/modules/prizes.js
@@ -2,16 +2,15 @@ export default () => {
   const screenPrizes = document.querySelector(`.screen--prizes`);
   const journeysImg = document.querySelector(`.prizes__item--journeys img`);
 
-  const journeysImgSrc = journeysImg.src;
-  console.log(journeysImgSrc);
+  const originalJourneysImgSrc = journeysImg.src;
 
   const resetAnimation = (mutationsList) => {
     for (let mutation of mutationsList) {
       if (mutation.type === `attributes` && mutation.attributeName === `class`) {
         if (!mutation.target.classList.contains(`active`)) {
-          journeysImg.src = journeysImgSrc;
+          journeysImg.src = originalJourneysImgSrc;
         } else {
-          journeysImg.src = `${journeysImgSrc}#${new Date().getTime()}`;
+          journeysImg.src = `${originalJourneysImgSrc}#${new Date().getTime()}`;
         }
       }
     }
